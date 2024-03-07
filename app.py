ebf71f2e5ff82e3885fe52642e4d4dbd993511ca
Pedro Henrique Lopes Vieira <pedro.lopes@tvmorena.com.br>
2024-03-07 13:24:16 -0400

    refactore

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy
+import openpyxl
+import nbformat
+import ipykernel
+import plotly
+
+
+# Passo a passo do projeto
+# Passo 1: Importar a base de ddados de Clientes
+# Passo 2: Visualizar a base de dados
+# Passo 3: Corrigir os problemas da base de dados
+# Passo 4: Analise dos cancelamentos
+# Passo 5: Analise da causa do cancelamento
+# Passo 6: Analise dos dados de entrada
